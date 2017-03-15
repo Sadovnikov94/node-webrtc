1be605c81bbc0a66e0005a0a37ffc249cdebd2ea
isadounikau <isadovnikov@qulix.com>
2017-03-15 12:51:47 +0300

    add tracing

--- src/common.h
+++ src/common.h
@@ -9,7 +9,7 @@
 #define WARN(msg) fprintf(stdout, "\033[01;33m native:%s \033[00m\n", msg)
 #define ERROR(msg) fprintf(stdout, "\033[01;32m native:%s \033[00m\n", msg)
 #define INFO(msg) fprintf(stdout, "\033[01;34m native:%s \033[00m\n", msg)
-
+#define TRACING
 #if defined(TRACING)
 
 #define TRACE(msg) printf("   TRACE: %s\n", msg)
